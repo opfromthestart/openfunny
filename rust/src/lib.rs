b86f12b505f2fb936b0c9550db974971090ea115
opfromthestart <opfromthestart@gmail.com>
2023-06-27 17:43:21 -0400

    Update files

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,6 +1,13 @@
-use jni::{sys::jstring, JNIEnv};
+use jni::JNIEnv;
+use jni::sys::jstring;
+use jni::objects::JObject;
 
 #[no_mangle]
-pub extern "C" fn test(env: JNIEnv) -> jstring {
-    env.new_string("hi".to_string()).unwrap().into_raw()
+#[allow(non_snake_case)]
+pub extern "C" fn Java_com_github_opfromthestart_openfunny_Scraper_getString<'local>(
+    env: JNIEnv,
+    _obj: JObject<'local>,
+) -> jstring {
+    let s = String::from("Hello from Rust");
+    env.new_string(s).unwrap().into_raw()
 }
